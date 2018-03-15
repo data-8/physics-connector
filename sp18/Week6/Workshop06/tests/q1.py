8ee54c843061e5bc6f5bb83df4bfe6a6a3b07a83
Kevin Chien <kevinchien17@gmail.com>
2018-03-15 15:36:38 -0700

    Added Workshop06 with okpy submission

---
+++ sp18/Week6/Workshop06/tests/q1.py
@@ -0,0 +1,23 @@
+test = {
+  'name': 'Question 1_',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> assert type(workshop_length) == int
+          >>> 1 <= workshop_length <= 5
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
