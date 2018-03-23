7a47577ab4b4f7a38c54590652445e551966b184
Kevin Chien <kevinchien17@gmail.com>
2018-03-23 11:50:58 -0700

    Workshop 7 fixes to ok and tests

--- sp18/Week7/Workshop07/tests/q1.py
+++ sp18/Week7/Workshop07/tests/q1.py
@@ -6,8 +6,10 @@ test = {
       'cases': [
         {
           'code': r"""
-          >>> assert type(numerical_inegral) == float # result is not a float
-          True
+          >>> assert type(q5_integral_10_samples) == float # result is not a float
+          >>> assert type(q5_integral_100_samples) == float # result is not a float
+          >>> assert type(q5_integral_1000_samples) == float # result is not a float
+          >>> assert type(q5_integral_quad) == float # result is not a float
           """,
           'hidden': False,
           'locked': False
