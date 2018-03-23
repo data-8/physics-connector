da6f86926ef04e56255c73e4cea908a80b4d0f7a
Kevin Chien <kevinchien17@gmail.com>
2018-03-23 10:19:26 -0700

    Workshop 07 OkPy

---
+++ sp18/Week7/Workshop07/tests/q1.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 1_',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> assert type(numerical_inegral) == float # result is not a float
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
